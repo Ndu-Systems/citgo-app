3182934571c455a5f27cbf06698d87d2b5db0ca7
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-04-14 21:44:00 +0200

    form set up

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { HomeRoutingModule } from './home-routing.module';
+import { FormPersolDetailsComponent } from "./client-sign-up/form-persol-details/form-persol-details.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home.component";
+import { HomeRoutingModule } from "./home-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ],
-  declarations: [HomeComponent]
+  imports: [CommonModule, HomeRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [HomeComponent, FormPersolDetailsComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
