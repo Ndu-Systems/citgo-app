b30dd53b72f6956ee5297efcf1416ca418b479cc
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-08 14:00:50 +0200

    profit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,7 +15,6 @@ export class AuthGuard implements CanActivate {
     private routeTo: Router
   ) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    debugger;
     const currentUser = this.authenticationService.currentUserValue;
     if (currentUser) {
       
---
+++ src/app/models/money.model.ts
@@ -0,0 +1,5 @@
+export interface Money{
+    currency:string;
+    rand:string;
+    cents:string;
+}
