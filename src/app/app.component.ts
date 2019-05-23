b20ffcbf838bcf53a420cdf4beb1ddbd86af14b4
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-23 09:51:38 +0200

    added confirm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { WEB_HOST, IS_LOCAL } from './shared/config';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'citgo-app';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    let router = window.location.href;
+    if (!router.includes("https") && !IS_LOCAL) {
+      window.location.href = WEB_HOST;
+    }
+  }
+  title = "citgo-app";
 }
