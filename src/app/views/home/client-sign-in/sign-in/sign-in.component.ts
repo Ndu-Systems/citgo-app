b5e207aacef31c3fc0b142c25ae15492befcfbb7
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-23 09:55:23 +0200

    added confirm

--- src/app/views/home/client-sign-in/sign-in/sign-in.component.ts
+++ src/app/views/home/client-sign-in/sign-in/sign-in.component.ts
@@ -1,60 +1,67 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { first } from 'rxjs/operators';
-import { AuthenticateService } from 'src/app/services/user/authenticate.service';
-import { LoginProcessService } from 'src/app/services/app-state/login-process.service';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { first } from "rxjs/operators";
+import { AuthenticateService } from "src/app/services/user/authenticate.service";
+import { LoginProcessService } from "src/app/services/app-state/login-process.service";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"]
 })
 export class SignInComponent implements OnInit {
   rForm: FormGroup;
   loading = false;
-  error = ''; 
+  error = "";
   isLogin: boolean = true;
   constructor(
     private fb: FormBuilder,
     private routeTo: Router,
     private loginService: AuthenticateService,
     private loginProcess: LoginProcessService
-  ) {
-  
-  }
+  ) {}
 
   ngOnInit() {
     this.rForm = this.fb.group({
-      email: new FormControl('magwaza@mail.com', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      password: ['pass@123!', Validators.required]
+      email: new FormControl(
+        "magwaza@mail.com",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ])
+      ),
+      password: ["pass@123!", Validators.required]
     });
   }
   closeModal() {
-  this.loginProcess.closeAll();
+    this.loginProcess.closeAll();
   }
 
   // convinient for easy form(rForm) data access
-  get f() { return this.rForm.controls; }
+  get f() {
+    return this.rForm.controls;
+  }
 
   signIn() {
     this.loading = true;
-    this.routeTo.navigate(['dashboard']);    
+    this.routeTo.navigate(["dashboard"]);
   }
   get formValues() {
     return this.rForm.controls;
   }
   Login() {
     // this.spinnerService.showSpinner();
- 
+
     this.loginService
       .loginUser(this.formValues.email.value, this.formValues.password.value)
       .pipe(first())
       .subscribe(response => {
-    
         if (response) {
           // this.router.navigate(["/dashboard"]);
           this.routeTo.navigate(["/dashboard"]);
@@ -65,9 +72,7 @@ export class SignInComponent implements OnInit {
       });
   }
 
-  resetPassword(){
+  resetPassword() {
     this.loginProcess.showChangePass();
   }
-
-
 }
