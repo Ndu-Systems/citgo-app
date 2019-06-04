0f841472c7696bce0f65002b3130323e9b381559
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-06-05 00:37:03 +0200

    admin ui

--- src/app/views/dashboard/investments/buy-share/buy-share.component.ts
+++ src/app/views/dashboard/investments/buy-share/buy-share.component.ts
@@ -18,6 +18,12 @@ export class BuyShareComponent implements OnInit {
   currentUser;
   investmentsList: Investment[]=[];
   canBuy:boolean= true;
+  explainContract: string;
+  widrawalDay: string;
+  profits: any[];
+  total: number;
+  widrawalDayAmount: number;
+  profit: number;
   constructor(
     private fb: FormBuilder,
     private authenticationService: AuthenticateService,
@@ -39,17 +45,61 @@ export class BuyShareComponent implements OnInit {
   ngOnInit() {
     this.currentUser = this.authenticationService.currentUserValue;
     this.rForm = this.fb.group({
-      Amount: ['', Validators.required],
+      Amount: [null, Validators.compose([Validators.required, Validators.min(5000), Validators.max(200000)])],
       Name: [`Share ${this.investmentsList.length+1}`, Validators.required],
       Type: ['', Validators.required],
       ClientId: [this.currentUser.ClientId]
     });
 
-   
+    this.rForm.valueChanges.subscribe(data => {
+ 
+      let capitalization = Number(data.Type);
+      let amount = data.Amount;
+      var today = new Date();
+
+      if (capitalization > 0) {
+        //contact
+        this.explainContract =
+          "Your shares will increase with 15% Compounded  Monthly";
+        var widrawDate = new Date(
+          today.setMonth(today.getMonth() + capitalization)
+        );
+        this.widrawalDay = `${this.formatDate(widrawDate)}`;
+        this.getCompoundGrowth(amount, capitalization);
+      } else {
+        // month ro month
+        this.explainContract =
+          "Your share value  will increase with 15% Monthly, No compound amount eligible";
+        var widrawDate = new Date(today.setMonth(today.getMonth() + 1));
+        this.widrawalDay = `${this.formatDate(widrawDate)}`;
+        this.geFlatGrowth(amount);
+      }
+    
+    });
   }
 
   closeModal() {
     this.buySharesProcessService.closeBuyShares()
+  }
+
+  getCompoundGrowth(amount: number, months: number) {
+    this.profits = [];
+
+    if (!amount || !months) return false;
+    this.total = amount;
+    for (let i = 0; i < 12; i++) {
+      this.total += this.total * 0.15;
+      if (i === months) {
+        this.widrawalDayAmount = Math.abs(amount - this.total);
+      }
+      this.profits.push(Math.round(this.total));
+    }
+    if(months == 12){
+      this.widrawalDayAmount = Math.abs(amount - this.total);
+
+    }
+    this.profit = Math.abs(amount - this.total);
+ 
   }
 
 
@@ -77,5 +127,42 @@ export class BuyShareComponent implements OnInit {
        }
     })
     this.closeModal();
+  }
+  monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  formatDate(date: Date) {
+    return (
+      date.getDay() +
+      " " +
+      this.monthNames[date.getMonth()] +
+      " " +
+      date.getFullYear()
+    );
+  }
+  geFlatGrowth(amount: number) {
+    this.profits = [];
+    if (!amount) return false;
+    this.total = amount;
+    for (let i = 0; i < 12; i++) {
+      this.total += amount * 0.15;
+      this.profits.push(Math.round(this.total));
+
+      if (i === 0) {
+        this.widrawalDayAmount = Math.abs(amount - this.total);
+      }
+    }
+    this.profit = Math.abs(amount - this.total);
   }
 }
