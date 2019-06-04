3f089a26c3af1976d4f12fc04e5aadc3b1f92de7
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-06-04 23:53:59 +0200

    admin ui

--- src/app/views/dashboard/admin-components/admin-nav/admin-nav.component.ts
+++ src/app/views/dashboard/admin-components/admin-nav/admin-nav.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Client, ExitModalEventEmmiter } from 'src/app/models';
-import { User } from 'src/app/models/user';
-import { Router } from '@angular/router';
-import { AuthenticateService, DocumentsService, CleintService, NotificationProcessService } from 'src/app/services';
-import { WEB_HOST, REFERALLINK } from 'src/app/shared/config';
-import { UserNotification } from 'src/app/models/processes/notification.process.model';
+import { Component, OnInit } from "@angular/core";
+import { Client, ExitModalEventEmmiter } from "src/app/models";
+import { User } from "src/app/models/user";
+import { Router } from "@angular/router";
+import {
+  AuthenticateService,
+  DocumentsService,
+  CleintService,
+  NotificationProcessService
+} from "src/app/services";
+import { WEB_HOST, REFERALLINK } from "src/app/shared/config";
+import { UserNotification } from "src/app/models/processes/notification.process.model";
 
 @Component({
-  selector: 'app-admin-nav',
-  templateUrl: './admin-nav.component.html',
-  styleUrls: ['./admin-nav.component.scss']
+  selector: "app-admin-nav",
+  templateUrl: "./admin-nav.component.html",
+  styleUrls: ["./admin-nav.component.scss"]
 })
 export class AdminNavComponent implements OnInit {
+  client;
+  user: User;
+  constructor(
+    private authenticateService: AuthenticateService,
+    private cleintService: CleintService
+  ) {
+    this.user = this.authenticateService.currentUserValue;
+  }
   ngOnInit(): void {
-    
+    this.cleintService.getClientById(this.user.ClientId).subscribe(r => {
+      this.client = r;
+    });
   }
   showMobileNav: boolean;
   openMobileNav() {
@@ -22,6 +37,4 @@ export class AdminNavComponent implements OnInit {
   closeMobileNav() {
     this.showMobileNav = false;
   }
-
-
 }
