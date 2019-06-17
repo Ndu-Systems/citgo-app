a3e9411e13d32877774489049925f9add4664568
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-06-17 21:49:04 +0200

    remove share option

--- src/app/views/dashboard/investments/investments.component.ts
+++ src/app/views/dashboard/investments/investments.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { Investment } from "src/app/models";
-import { InvestmentService, AuthenticateService,NotificationProcessService } from "src/app/services";
+import {
+  InvestmentService,
+  AuthenticateService,
+  NotificationProcessService
+} from "src/app/services";
 import { User } from "src/app/models/user";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { SHARE_REMOVED } from "src/app/shared/config";
 
 @Component({
   selector: "app-investments",
@@ -16,10 +22,14 @@ export class InvestmentsComponent implements OnInit {
     private investmentService: InvestmentService,
     private authenticationService: AuthenticateService,
     private notificationProcessService: NotificationProcessService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
   ) {}
 
   ngOnInit() {
-
+ this.init();
+  }
+  init(){
     this.user = this.authenticationService.currentUserValue;
     this.investmentService
       .getInvestmentsByClientId(this.user.ClientId)
@@ -29,16 +39,33 @@ export class InvestmentsComponent implements OnInit {
         }
       });
 
-      this.investmentService.castClientshares.subscribe(val => {
-        if(val){
-          this.investmentsList = val;
-        }
-      });
-  
+    this.investmentService.castClientshares.subscribe(val => {
+      if (val) {
+        this.investmentsList = val;
+      }
+    });
   }
-  oplaodPOP(data:Investment){
+  oplaodPOP(data: Investment) {
     this.notificationProcessService.showUplaod();
     this.notificationProcessService.updateInvestementId(data.InvestmentId);
   }
- 
+  remove(investment: Investment) {
+    this.confirmationService.confirm({
+      message: `You are about to delete  ${investment.Name}, continue?`,
+      accept: () => {
+        investment.StatusId = SHARE_REMOVED;
+        this.investmentService.updateInvestment(investment).subscribe(r => {
+        this.init();
+
+          this.messageService.add({
+            life: 7000,
+            severity: "info",
+            summary: "Info",
+            detail: "Share was removed successfully"
+          });
+  
+        });
+      }
+    });
+  }
 }
