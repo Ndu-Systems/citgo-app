218242128fc021b9936d54b2e05c8cd2d7eca89b
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-06-17 17:32:59 +0200

    buy shares re worked

--- src/app/views/dashboard/investments/payment/payment.component.ts
+++ src/app/views/dashboard/investments/payment/payment.component.ts
@@ -1,3 +1,5 @@
+import { InvestmentService } from 'src/app/services';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
+  isDone= false;
+  investmentId: string;
+  amount: number;
+  constructor(private router:Router, private investmentService:InvestmentService, private activatedRoute: ActivatedRoute,
+    ) {
+      this.activatedRoute.params.subscribe(r => {
+        this.investmentId = r["id"];
+        this.investmentService.getInvestmentsById(this.investmentId).subscribe(r=>{
+          this.amount = r.Amount;
+        })
+      });
+     }
 
   ngOnInit() {
   }
-
+next(){
+  this.isDone = true
+}
+done(){
+  this.isDone = false
+ this.router.navigate(['dashboard']);
+}
 }
