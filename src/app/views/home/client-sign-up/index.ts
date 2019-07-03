56f9dcf9972db0510830526469d205f40d8aa987
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-07-03 23:25:34 +0200

    home refined

--- src/app/views/home/client-sign-up/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './form-persol-details/form-persol-details.component';
-export * from './form-banking-details/form-banking-details.component';
-export * from './form-beneficiaries/form-beneficiaries.component';
-
