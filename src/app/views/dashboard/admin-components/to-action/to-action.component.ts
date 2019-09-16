dfdfbbb35caf47c71bd514cfb4aa5074cdb5d176
Ndumiso Mthembu <Ndumiso.Mthembu@dimensiondata.com>
2019-09-16 21:56:45 +0200

    citgo profrit before payment bug fix

--- src/app/views/dashboard/admin-components/to-action/to-action.component.ts
+++ src/app/views/dashboard/admin-components/to-action/to-action.component.ts
@@ -1,28 +1,28 @@
 import { SpinnerProcessService } from './../../../../services/app-state/spinner-process.service';
-import { Bonus } from "./../../../../models/bonus.model";
-import { CleintService } from "./../../../../services/dashboard/cleint.service";
-import { MessageService } from "primeng/api";
+import { Bonus } from './../../../../models/bonus.model';
+import { CleintService } from './../../../../services/dashboard/cleint.service';
+import { MessageService } from 'primeng/api';
 import {
   SHARE_PENDING_VERFICATION,
   SHARE_ACTIVE,
   BONUS_PERCENT
-} from "./../../../../shared/config";
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { InvestmentService } from "src/app/services";
-import { Investment, Client } from "src/app/models";
-import { BonusService } from "src/app/services/dashboard/bonus.service";
+} from './../../../../shared/config';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InvestmentService } from 'src/app/services';
+import { Investment, Client } from 'src/app/models';
+import { BonusService } from 'src/app/services/dashboard/bonus.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-to-action",
-  templateUrl: "./to-action.component.html",
-  styleUrls: ["./to-action.component.scss"]
+  selector: 'app-to-action',
+  templateUrl: './to-action.component.html',
+  styleUrls: ['./to-action.component.scss']
 })
 export class ToActionComponent implements OnInit {
   shares$: Observable<Investment[]>;
   statusId: any;
-  search:string;
+  search: string;
   constructor(
     private investmentService: InvestmentService,
     private bonusService: BonusService,
@@ -33,7 +33,7 @@ export class ToActionComponent implements OnInit {
   ) {
     this.activatedRoute.params.subscribe(r => {
       this.statusId = r[`id`];
-      this.shares$ =  this.investmentService.getInvestmentsByStatus(this.statusId || SHARE_PENDING_VERFICATION)
+      this.shares$ =  this.investmentService.getInvestmentsByStatus(this.statusId || SHARE_PENDING_VERFICATION);
     });
   }
 
@@ -42,44 +42,44 @@ export class ToActionComponent implements OnInit {
     data.StatusId = SHARE_ACTIVE;
     this.spinnerProcessService.showSpinner();
     this.investmentService.updateInvestment(data).subscribe(res => {
-      let updatedInvestment: Investment = res;
-      if (updatedInvestment.StatusId == SHARE_ACTIVE) {
+      const updatedInvestment: Investment = res;
+      if (updatedInvestment.StatusId === SHARE_ACTIVE) {
         this.messageService.add({
           life: 7000,
-          severity: "success",
-          summary: "Now active! ",
-          detail: "Share approved!"
+          severity: 'success',
+          summary: 'Now active! ',
+          detail: 'Share approved!'
         });
 
-        //update list 
-        this.shares$= this.investmentService.getInvestmentsByStatus(SHARE_PENDING_VERFICATION);
+        // update list
+        this.shares$ = this.investmentService.getInvestmentsByStatus(SHARE_PENDING_VERFICATION);
         this.spinnerProcessService.closeSpinner();
 
-        //check if client have a parent
+        // check if client have a parent
         this.cleintService
           .getClientById(updatedInvestment.ClientId)
           .subscribe(clientRes => {
-            let client: Client = clientRes;
+            const client: Client = clientRes;
             if (client.ClientId && client.ParentId) {
 
-              //update the current list
+              // update the current list
 
-              let bonus: Bonus = {
+              const bonus: Bonus = {
                 Amount: updatedInvestment.Amount * BONUS_PERCENT,
                 ClientId: client.ParentId,
                 ParentId: updatedInvestment.ClientId,
-                CreateUserId: "SYS",
-                ModifyUserId: "SYS",
+                CreateUserId: 'SYS',
+                ModifyUserId: 'SYS',
                 StatusId: 1
               };
 
               // grant a bonus
               this.bonusService.addBonus(bonus).subscribe(cli => {
-                let bunusReceiver: Client = cli;
+                const bunusReceiver: Client = cli;
                 this.messageService.add({
                   life: 7000,
-                  severity: "success",
-                  summary: "Now active! ",
+                  severity: 'success',
+                  summary: 'Now active! ',
                   detail: `Referral bonus of R ${updatedInvestment.Amount *
                     BONUS_PERCENT}  granted to ${bunusReceiver.FirstName} ${
                     bunusReceiver.Surname
--- src/app/views/home/client-sign-in/sign-in/sign-in.component.ts
+++ src/app/views/home/client-sign-in/sign-in/sign-in.component.ts
@@ -1,33 +1,33 @@
-import { SpinnerProcessService } from "./../../../../services/app-state/spinner-process.service";
-import { MessageService } from "primeng/api";
-import { SignUpProcessService } from "src/app/services/app-state/sign-up-process.service";
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { SpinnerProcessService } from './../../../../services/app-state/spinner-process.service';
+import { MessageService } from 'primeng/api';
+import { SignUpProcessService } from 'src/app/services/app-state/sign-up-process.service';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
   FormControl,
   Validators
-} from "@angular/forms";
-import { Router } from "@angular/router";
-import { first } from "rxjs/operators";
-import { LoginProcessService } from "src/app/services/app-state/login-process.service";
-import { AuthenticateService } from "src/app/services/home/user/authenticate.service";
-import { REMBERPASSWORD } from "src/app/shared/config";
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { LoginProcessService } from 'src/app/services/app-state/login-process.service';
+import { AuthenticateService } from 'src/app/services/home/user/authenticate.service';
+import { REMBERPASSWORD } from 'src/app/shared/config';
 
 @Component({
-  selector: "app-sign-in",
-  templateUrl: "./sign-in.component.html",
-  styleUrls: ["./sign-in.component.scss"]
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
   rForm: FormGroup;
   loading = false;
-  error = "";
-  isLogin: boolean = true;
+  error = '';
+  isLogin = true;
 
-  email:string = "";
-  password:string = ""
-  savePassword:boolean = false;
+  email = '';
+  password = '';
+  savePassword = false;
   constructor(
     private fb: FormBuilder,
     private routeTo: Router,
@@ -37,21 +37,21 @@ export class SignInComponent implements OnInit {
     private messageService: MessageService,
     private spinnerProcessService: SpinnerProcessService
   ) {
-    let data =  localStorage.getItem(REMBERPASSWORD);
-    if( data !== null && data !== "null"){
-    this.email= JSON.parse(data).email;
-    this.password = JSON.parse(data).password;
-    this.savePassword = true;
+    const data = localStorage.getItem(REMBERPASSWORD);
+    if (data !== null && data !== 'null') {
+      this.email = JSON.parse(data).email;
+      this.password = JSON.parse(data).password;
+      this.savePassword = true;
     }
   }
 
   ngOnInit() {
     this.rForm = this.fb.group({
       email: new FormControl(
-       this.email,
+        this.email,
         Validators.compose([
           Validators.required,
-          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
         ])
       ),
       password: [this.password, Validators.required],
@@ -72,10 +72,10 @@ export class SignInComponent implements OnInit {
   }
   Login() {
     // this.spinnerService.showSpinner();
-    if(this.formValues.savePassword.value){
-      localStorage.setItem(REMBERPASSWORD, JSON.stringify({email:this.formValues.email.value,password:this.formValues.password.value }))
-    }else{
-      localStorage.setItem(REMBERPASSWORD,null);
+    if (this.formValues.savePassword.value) {
+      localStorage.setItem(REMBERPASSWORD, JSON.stringify({ email: this.formValues.email.value, password: this.formValues.password.value }));
+    } else {
+      localStorage.setItem(REMBERPASSWORD, null);
     }
     this.spinnerProcessService.showSpinner();
     this.loginService
@@ -84,25 +84,34 @@ export class SignInComponent implements OnInit {
       .subscribe(
         response => {
           this.spinnerProcessService.closeSpinner();
-
+          if (Number(response.ClientStatus) === 10) {
+            this.messageService.add({
+              life: 10000,
+              severity: 'error',
+              summary: 'opps account blocked!',
+              detail:
+                'Please Contact Your System Administrator at queries@citgoafrica.com'
+            });
+            return null;
+          }
           if (response.UserId) {
-            this.routeTo.navigate(["/dashboard"]);
+            this.routeTo.navigate(['/dashboard']);
           } else {
             // this.error = response;
             this.messageService.add({
               life: 7000,
-              severity: "warn",
-              summary: "access denied!",
+              severity: 'warn',
+              summary: 'access denied!',
               detail:
-                "Please make sure you enter your email address and password correctly"
+                'Please make sure you enter your email address and password correctly'
             });
           }
         },
         error => {
           this.messageService.add({
             life: 12000,
-            severity: "error",
-            summary: "Newtork error occured!",
+            severity: 'error',
+            summary: 'Newtork error occured!',
             detail: error
           });
           this.spinnerProcessService.closeSpinner();
